bbe36d38d67f732799e35db477e58d89645421f8
Joshua Hynes <jhynes@stackoverflow.com>
2016-07-18 13:51:17 -0400

    Rough prototype looking at navigation.

--- careers-ui/js/main.js
+++ careers-ui/js/main.js
@@ -1,1 +1,23 @@
 
+/* Scroll navigation */
+$(".js-nav-content").on("click", function() {
+	$(".nav-main").addClass("js-offpage");
+	$(".nav-company").addClass("js-focus").removeClass("js-offpage");
+});
+$(".js-nav-design").on("click", function() {
+	$(".nav-main").addClass("js-offpage");
+	$(".nav-design").addClass("js-focus").removeClass("js-offpage");
+});
+$(".js-nav-analytics").on("click", function() {
+	$(".nav-main").addClass("js-offpage");
+	$(".nav-analytics").addClass("js-focus").removeClass("js-offpage");
+});
+$(".js-nav-settings").on("click", function() {
+	$(".nav-main").addClass("js-offpage");
+	$(".nav-settings").addClass("js-focus").removeClass("js-offpage");
+});
+
+$(".-home").on("click", function() {
+	$(this).parent().parent().addClass("js-offpage").removeClass("js-focus");
+	$(".nav-main").addClass("js-focus").removeClass("js-offpage");
+});
