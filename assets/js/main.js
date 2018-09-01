45046fd834b011e93a0bd3cbea5aec2b1bdca0de
Joshua Hynes <jhynes@stackoverflow.com>
2018-09-01 15:00:16 -0400

    Roughing out a new website. Still exploring ideas.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-// Test
+$(document).ready(function() {
+    var toc = $(".js-page-toc");
+    toc.scrollToFixed({ marginTop: 512 });
+
+    // var tocMenu = $(".jh-toc--menu");
+    // var tocTitle = $(".js-toc--title");
+    // var tocLabel = $(".js-toc--label");
+    // var tocNumber = $(".js-toc--number");
+    // var tocList = $("grid--cell js-toc--item");
+    // var tocItem = getElementbyId
+
+    // tocList.appendTo(tocMenu);
+});
